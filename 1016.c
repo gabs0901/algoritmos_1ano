0acd895af9b0417f49a5bb00509eac79776cf5f7
gabs <90000742+gabs0901@users.noreply.github.com>
2026-04-27 03:48:05 -0300

    Feito em 27/04/2026

---
+++ 1016.c
@@ -0,0 +1,16 @@
+//Solução do problema 1016 (Beecrowd)
+
+#include <stdio.h>
+
+int main (){
+	int d, d1; //distancia
+			
+	scanf("%d", &d);
+	
+	d1 = d * 2;
+	
+	printf("%d minutos\n", d1);
+	
+	return 0;
+
+}
