0a47d664c92e6ce49b228c8318d1f068a803651a
gabs <90000742+gabs0901@users.noreply.github.com>
2026-05-04 23:02:53 -0300

    Fix calculation and output for student grades

--- 1040.c
+++ 1040.c
@@ -3,26 +3,25 @@
 
 int main(){
     //declaração de variáveis
-    float n1, n2, n3, n4, media, exame; //variáveis de entrada
+    float n1, n2, n3, n4, media, newMedia, exame; //variáveis de entrada
     
-
     //leitura de variaveis
     scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
 
     //calculo
-    media = (n1*2 + n2*3 + n3*4 + n4*1)/10
-    if ( media >= 7.0)
-        printf("Aluno aprovado.");
-    else if (m < 5.0){
-        printf("Aluno reprovado");
-    else if (m > 5.0 || < 6.9)
-        printf("Aluno em exame");
-        scanf("%f", media);
-        
-    
-
-
-
-    
+    media = (n1*2 + n2*3 + n3*4 + n4*1)/10;
+    if (media >= 7.0)
+        printf("Media: %.1f\nAluno aprovado.\n", media);
+    else if (media < 5.0)
+        printf("Media: %.1f\nAluno reprovado.\n", media);
+    else if (media > 5.0 || media < 6.9){
+        printf("Media: %.1f\nAluno em exame.\n", media);
+		scanf("%f", &exame);
+        newMedia = (media + exame)/2;
+        if (newMedia >= 5.0)
+        	printf("Nota do exame: %.1f\nAluno aprovado.\nMedia final: %.1f\n", exame, newMedia);
+        else if (newMedia <= 4.9)
+            printf("Aluno reprovado.\nMedia final: %.1f\n", newMedia);	
+        }
     return 0;
 }
