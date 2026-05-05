159be9ae1545d0b7163044feb31ea883e7da8978
gabs <90000742+gabs0901@users.noreply.github.com>
2026-05-04 23:03:47 -0300

    Implement sorting for three integers
    Refactor sorting logic and improve variable handling.

--- 1042.c
+++ 1042.c
@@ -1,17 +1,48 @@
-//Solução do problema 1042(Beecrowd)
 #include <stdio.h>
 
-int main (){
-    
+int main() {
+   	//declaracao de variaveis
     int a, b, c;
-           
+    
+    //leitura de variaveis
     scanf("%d %d %d", &a, &b, &c);
     
-    if (a > b)        //regra matematica: se X < Y < Z
-        x = a;         //testa se A > B, se for, troca de lugar com o B 
-        a = b;       
-        b = x;
-    else if ()
-
-
+   									
+    if (a < b && a < c) {				
+        printf("%d\n", a);			
+        if (b < c) {					
+            printf("%d\n", b);
+            printf("%d\n", c);
+        } else {
+            printf("%d\n", c);
+            printf("%d\n", b);
+        }
+    } 
+    else if (b < a && b < c) {
+        printf("%d\n", b);
+        if (a < c) {
+            printf("%d\n", a);
+            printf("%d\n", c);
+        } else {
+            printf("%d\n", c);
+            printf("%d\n", a);
+        }
+    } 
+    else {
+        printf("%d\n", c);
+        if (a < b) {
+            printf("%d\n", a);
+            printf("%d\n", b);
+        } else {
+            printf("%d\n", b);
+            printf("%d\n", a);
+        }
+    }
+    
+    printf("\n");
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    
+    return 0;
 }
